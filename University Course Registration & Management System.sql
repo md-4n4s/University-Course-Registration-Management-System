70e8385aa808e846c1707b0c7c3fa8d3888a322c
md-4n4s <m.d.4n4s@gmail.com>
2026-07-15 19:58:00 +0500

    Created student and teacher table

--- University Course Registration & Management System.sql
+++ University Course Registration & Management System.sql
@@ -1,2 +1,27 @@
 create database universitySystem;
 use universitySystem;
+
+create table Student(
+	studentID int primary key,
+    studentName varchar(100) not null,
+    studentGender varchar(50) not null,
+    studentDOB date not null,
+    studentPhone int unique,
+    studentEmail varchar(100) unique,
+    studentAddress text not null,
+    studentAdmissionDate date
+);
+
+alter table Student
+modify studentPhone varchar(50);
+
+create table Teacher(
+	teacherID int primary key,
+    teacherName varchar(100) not null,
+    teacherQualification text not null,
+    teacherSalary int not null,
+    teacherPhone varchar(50) unique,
+    teacherEmail varchar(100) unique,
+    teacherDepartment varchar(100) not null
+);
+
